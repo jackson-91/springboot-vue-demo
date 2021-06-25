759ba1476f508f8ce60ccebfe71d885cc5e9d3ce
谷歌独行侠 <1152232809@qq.com>
2021-06-25 22:59:26 +0800

    修改

--- springboot/dev-admin/src/main/java/org/dev/app/SpringBootVueApplication.java
+++ springboot/dev-admin/src/main/java/org/dev/app/SpringBootVueApplication.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-//import serilogj.Log;
-//import serilogj.LoggerConfiguration;
-//
-//import static serilogj.sinks.coloredconsole.ColoredConsoleSinkConfigurator.*;
-//import static serilogj.sinks.rollingfile.RollingFileSinkConfigurator.*;
-//import static serilogj.sinks.seq.SeqSinkConfigurator.*;
 
 @SpringBootApplication
 
@@ -23,15 +17,5 @@ public class SpringBootVueApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootVueApplication.class, args);
-        //Log.setLogger(new LoggerConfiguration()
-        //        .writeTo(seq("http://114.67.109.212:5341"))
-        //        .createLogger());
-        //for (int i = 0; i < 10; i++) {
-        //    Log.information("Hello from {lang}!index={index}", "Java", i);
-        //    Log.debug("Hello from {lang}!index={index}", "Java", i);
-        //    Log.warning("Hello from {lang}!index={index}", "Java", i);
-        //    Log.error("Hello from {lang}!index={index}", "Java", i);
-        //    Log.debug("Hello from {lang}!index={index}", "Java", i);
-        //}
     }
 }
